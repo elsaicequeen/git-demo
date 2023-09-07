1c71be195b72cb39def76c16a5865f8deb03bf67
Elsaicequeen <anish.majumdar123@gmail.com>
2023-09-07 10:39:06 +0530

    second message

---
+++ pythoon.py
@@ -0,0 +1,18 @@
+variable_1 = "really"
+print ("I "+ variable_1 + " have aids")
+print ("hello world, time to die?")
+print(type(variable_1))
+def aids_function (a,b,c):
+    """to bamboozle all function users"""
+    a="8"
+    b= b*"="
+    c= "|)"
+    return a+b+c
+print (aids_function(5,8,7))
+help(aids_function)
+"""None is NULL in python"""
+def mod_5(x):
+    return x % 5
+    
+
+print (max (1,5,7,55,44,43,67,key = mod_5))
